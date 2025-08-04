88c37201691df6172fd141ee1237fe0d020c9359
Mpho Matli <mphojames9@gmail.com>
2025-08-04 16:06:26 +0200

    update

--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@ let items = JSON.parse(localStorage.getItem('inventory')) || [];
 function saveToStorage() {
   localStorage.setItem('inventory', JSON.stringify(items));
 }
+
+function renderItems(filteredItems = items) {
+  inventoryList.innerHTML = '';
+  const grouped = filteredItems.reduce((acc, item, index) => {
+    if (!acc[item.category]) acc[item.category] = [];
+    acc[item.category].push({ ...item, index });
+    return acc;
+  }, {});
+}
